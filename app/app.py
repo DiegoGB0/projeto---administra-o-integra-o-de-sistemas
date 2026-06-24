4627c226bcf85dcdf233c306d33ae8db8c3017c6
Kaue5416 <kauemarinho05@gmail.com>
2026-06-24 12:02:08 -0300

    Atualizações

--- app/app.py
+++
@@ -1,6 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-server = HTTPServer(("0.0.0.0", 5000), SimpleHTTPRequestHandler)
-
-print("Servidor iniciado na porta 5000")
-server.serve_forever()
